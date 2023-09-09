43ddebf0dd6654456fd770f6f23e4f5d5cd1a101
dhruv soni <dhruvsoni1560.ds@gmail.com>
2023-09-09 02:05:02 -0400

    s

--- src/main/java/dev/dhruv/spendingtracker/SpendingtrackerApplication.java
+++ src/main/java/dev/dhruv/spendingtracker/SpendingtrackerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpendingtrackerApplication {
 
+	// https://git.heroku.com/spendingtracker.git
+	// https://spendingtracker-22d1d75eaad7.herokuapp.com/
 	public static void main(String[] args) {
 		SpringApplication.run(SpendingtrackerApplication.class, args);
 	}
